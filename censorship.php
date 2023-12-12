fc9829c1657849ae71bfb991eb39029274bc9000
simonemassenti <s.massenti96@gmail.com>
2023-12-12 10:08:12 +0100

    update: badword

--- censorship.php
+++ censorship.php
@@ -3,6 +3,11 @@
 $paragraph = $_GET["paragraph"];
 $badword = $_GET["badword"];
 
-echo str_replace($badword, "***", $paragraph);
+echo "<h2>Testo censurato</h2>";
+echo "<h4>".str_replace($badword, "***", $paragraph)."</h4>";
+echo "<h2>Testo originale</h2>";
+echo "<h4>".$paragraph."</h4>";
+echo "<h2>Lunghezza testo:</h2>";
+echo "<h4>".strlen($paragraph)."</h4>";
 
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>CENSURA</h1>
     <form action="censorship.php" method="get">
@@ -19,4 +21,5 @@
 
 
 </body>
+
 </html>
